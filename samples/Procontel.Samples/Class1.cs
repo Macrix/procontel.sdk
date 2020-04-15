795c3432de92611f7993ed893f24264ee9622263
Jaroslaw Kurdzieko <jaroslaw.kurdzieko@macrix.pl>
2020-04-15 20:34:19 +0200

    add simple endpoints

--- samples/Procontel.Samples/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Procontel.Samples
-{
-  public class Class1
-  {
-  }
-}
---
+++ samples/SimpleEndpoints/EmptyEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using ProconTel.Sdk.Attributes;
+using ProconTel.Sdk.Builders;
+using ProconTel.Sdk.Services;
+using System.Threading.Tasks;
+
+namespace SingleEndpoint
+{
+  [EndpointMetadata(Name = "LifeTimeCycle", SupportedRoles = SupportedRoles.Both)]
+  public class LifeTimeCycleEndpoint : IEndpointLifeTimeCycle
+  {
+    private readonly ILogger _logger;
+    public LifeTimeCycleEndpoint(ILogger logger) => _logger = logger;
+
+    public Task InitializeAsync()
+    {
+      _logger.Information("Initialize");
+      return Task.CompletedTask;
+    }
+
+    public Task TerminateAsync()
+    {
+      _logger.Information("Terminate");
+      return Task.CompletedTask;
+    }
+  }
+}
