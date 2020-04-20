724340c2d730801b629b97b0ba27a63be5861f6d
Jaroslaw Kurdzieko <jaroslaw.kurdzieko@macrix.pl>
2020-04-20 15:23:14 +0200

    start working with winForms sample

---
+++ samples/VisualEndpoints.WinForms/Providers/WinFormsStatusControlProvider.cs
@@ -0,0 +1,28 @@
+﻿using ProconTel.Sdk.UI.Models;
+using ProconTel.Sdk.UI.Providers;
+using ProconTel.Sdk.UI.Services;
+using VisualEndpoints.WinForms.UI;
+
+namespace VisualEndpoints.WinForms.Providers
+{
+  public class WinFormsStatusControlProvider : IEndpointStatusControlProvider
+  {
+    public bool SupportsDialogOnlineUpgrade => true;
+
+    public bool SupportsCommunicationConsole => true;
+
+    private readonly IEndpointStatusControl _control;
+    private readonly IEndpointCommandSender _sender;
+    public WinFormsStatusControlProvider(IEndpointCommandSender sender)
+    {
+      _sender = sender;
+      _control = new WinFormsStatusControl(_sender);
+    }
+
+    public object GetStatusControl() => _control;
+
+    public EndpointStatusControlType GetStatusControlType() => EndpointStatusControlType.WinForms;
+
+    public IEndpointStatusControl GetStatusDialogController() => _control;
+  }
+}
---
+++ samples/VisualEndpoints.WinForms/WinFormsStatusEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using ProconTel.Sdk.Attributes;
+using ProconTel.Sdk.Builders;
+using ProconTel.Sdk.Messages;
+using ProconTel.Sdk.Services;
+using ProconTel.Sdk.UI.Attributes;
+using ProconTel.Sdk.UI.Models;
+using System;
+using System.Threading.Tasks;
+using VisualEndpoints.WinForms.Providers;
+using VisualEndpoints.WinForms.UI;
+
+namespace VisualEndpoints.WinForms
+{
+  [StatusControl(typeof(WinFormsStatusControl), EndpointStatusControlType.WinForms)]
+  //[StatusControlProvider(typeof(WinFormsStatusControlProvider))]
+  [EndpointMetadata(Name = "WinFormsStatus", SupportedRoles = SupportedRoles.None)]
+  public class WinFormsStatusEndpoint : ICommandHandler
+  {
+    private readonly ILogger _logger;
+    private readonly IRuntimeContext _runtimeContext;
+    public WinFormsStatusEndpoint(ILogger logger, IRuntimeContext runtimeContext)
+    {
+      _logger = logger;
+      _runtimeContext = runtimeContext;
+    }
+
+    public Task<object> HandleCommandAsync(object command, ICorrelationContext context = null)
+    {
+      _logger.Information($"Received command from status control {command}");
+      switch (command)
+      {
+        case "dowork": _logger.Information($"Let's do some work!"); break;
+        case "notify": _runtimeContext.NotificationService.NotifyUI($"Notify from { _runtimeContext.MetadataContext.Caption} ", false); break;
+        default: throw new NotSupportedException($"Command {command} is not supported.");
+      }
+      return Task.FromResult<object>("Done");
+    }
+  }
+}
