a9dfb01359214b6c3008091e4c8a0de4c9bea1c8
Jaroslaw Kurdzieko <jaroslaw.kurdzieko@macrix.pl>
2020-06-24 15:28:54 +0200

    fix wpf status sample

--- samples/VisualEndpoints.Wpf/UI/WpfStatusControl.xaml.cs
+++ samples/VisualEndpoints.Wpf/UI/WpfStatusControl.xaml.cs
@@ -10,26 +10,81 @@ namespace VisualEndpoints.Wpf.UI
 {
     public partial class WpfStatusControl : UserControl, IEndpointStatusControl
     {
+        private readonly IEndpointCommandSender _sender;
+        private readonly ILocalStorage _localStorage;
         private readonly ISecurityService _securityService;
         public WpfStatusControl() => InitializeComponent();
-        public WpfStatusControl(ISecurityService securityService) : this() => _securityService = securityService;
-
-        public void DisplayStatus(object statusInformation) { }
-        public void OnStatusControlHidden() { }
-        public void OnStatusControlShown() { }
-
-        public void Logout(object sender, System.Windows.RoutedEventArgs e)
+        public WpfStatusControl(IEndpointCommandSender sender, ILocalStorage localStorage, ISecurityService securityService) : this()
         {
-            _securityService.SignOut();
+            _sender = sender;
+            _localStorage = localStorage;
+            _securityService = securityService;
+        }
+        public void DisplayStatus(object statusInformation)
+        {
+            if (statusInformation != null)
+            {
+                txtNotifications.Text = txtNotifications.Text.Insert(0, $"{DateTime.Now.ToString("HH:mm:ss")} {statusInformation.ToString()}{Environment.NewLine}");
+            }
         }
 
-        public void Login(object sender, System.Windows.RoutedEventArgs e)
+        public void OnStatusControlHidden() { }
+
+        public void OnStatusControlShown()
         {
-            var isAdministrator = false;
-            var authorized = _securityService.Authenticate(hashLoginAdpassword);
-            if (authorized)
+            var theme = _localStorage.ReadValue<object>("theme");
+            cbxTheme.SelectedItem = cbxTheme.Items.OfType<ComboBoxItem>().SingleOrDefault(x => x.Content.Equals(theme));
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            txtConsole.Text = "Running...";
+            try
             {
-                isAdministrator = _securityService.IsInRole("administrator");
+                var commands = txtCommand.Text.Split(' ');
+                switch (commands.FirstOrDefault())
+                {
+                    case "login":
+                        {
+                            _securityService.SignOut();
+                            var auth = commands[1];
+                            var token = _securityService.Authenticate(auth);
+                            if (_securityService.IsAuthorized)
+                            {
+                                txtConsole.Text = $"Login successed.";
+                            }
+                            else
+                            {
+                                txtConsole.Text = $"Login failed.";
+                            }
+
+                            break;
+                        }
+                    case "isinrole":
+                        {
+                            var role = commands[1];
+                            txtConsole.Text = $"Is in role {role}: { _securityService.IsInRole(role)}";
+                            break;
+                        }
+                    default:
+                        {
+                            var result = _sender.SendCommandToServerEndpoint(txtCommand.Text);
+                            txtConsole.Text = result.ToString();
+                            break;
+                        }
+                }
+            }
+            catch (Exception ex)
+            {
+                txtConsole.Text = $"Something goes wrong. {ex.Message}";
+            }
+        }
+
+        private void cbxTheme_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbxTheme.SelectedItem is ComboBoxItem item)
+            {
+                _localStorage.WriteValue("theme", item.Content);
             }
         }
     }
