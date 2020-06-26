8757309c416a00b782c5e3345331ca7857d94e7a
Jaroslaw Kurdzieko <jaroslaw.kurdzieko@macrix.pl>
2020-06-26 15:01:26 +0200

    implement configuration dialog provider sample

---
+++ samples/VisualEndpoints.WinForms/GenericConfigurationEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using ProconTel.Sdk.Attributes;
+using ProconTel.Sdk.UI.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisualEndpoints.WinForms.Providers;
+
+namespace VisualEndpoints.WinForms
+{
+    [EndpointMetadata(Name = "Generic Configuration Endpoint Loosely Coupled", SupportedRoles = SupportedRoles.None)]
+    [ConfigurationDialogProvider("VisualEndpoints.WinForms", "GenericConfigurationDialogProvider")]
+    public class GenericConfigurationEndpointLooselyCoupled
+    {
+    }
+
+    [EndpointMetadata(Name = "Generic Configuration Endpoint", SupportedRoles = SupportedRoles.None)]
+    [ConfigurationDialogProvider(typeof(GenericConfigurationDialogProvider))]
+    public class GenericConfigurationEndpoint
+    {
+    }
+}
---
+++ samples/VisualEndpoints.WinForms/UI/GenericConfigurationDialog.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace VisualEndpoints.WinForms.UI
+{
+    partial class GenericConfigurationDialog<T>
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGeneric = new System.Windows.Forms.Label();
+            this.lblTypeName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblGeneric
+            // 
+            this.lblGeneric.AutoSize = true;
+            this.lblGeneric.Location = new System.Drawing.Point(12, 22);
+            this.lblGeneric.Name = "lblGeneric";
+            this.lblGeneric.Size = new System.Drawing.Size(70, 13);
+            this.lblGeneric.TabIndex = 3;
+            this.lblGeneric.Text = "Generic type:";
+            // 
+            // lblTypeName
+            // 
+            this.lblTypeName.AutoSize = true;
+            this.lblTypeName.Location = new System.Drawing.Point(88, 22);
+            this.lblTypeName.Name = "lblTypeName";
+            this.lblTypeName.Size = new System.Drawing.Size(0, 13);
+            this.lblTypeName.TabIndex = 4;
+            // 
+            // GenericConfigurationDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTypeName);
+            this.Controls.Add(this.lblGeneric);
+            this.Name = "GenericConfigurationDialog";
+            this.Text = "GenericConfigurationDialog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGeneric;
+        private System.Windows.Forms.Label lblTypeName;
+    }
+}
