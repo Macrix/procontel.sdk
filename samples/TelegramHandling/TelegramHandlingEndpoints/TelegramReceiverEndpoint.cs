bf70213294bb5f278357f26c94b89915538ec107
Dariusz Wilczak <dw@macrix.pl>
2021-07-08 16:41:57 +0200

    Added sample for sending and receiving telegrams

---
+++ samples/TelegramHandling/TelegramHandlingEndpoints/TelegramReceiverEndpoint.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using ProconTel.Sdk.Attributes;
+using ProconTel.Sdk.Builders;
+using ProconTel.Sdk.Messages;
+using ProconTel.Sdk.Services;
+using ProconTel.Sdk.StandardEndpoints;
+using TelegramDefinitions.Telegrams;
+
+namespace TelegramHandlingEndpoints
+{
+  [EndpointMetadata(Name = "Telegram Receiver", SupportedRoles = SupportedRoles.Subscriber)]
+  // In order to receive telegrams from ProconTEL Standard Endpoints make sure to
+  // support one of the protocols XML or Binary
+  [SupportsXmlProtocol, SupportsBinaryProtocol]
+  public class TelegramReceiverEndpoint : IHandler
+  {
+    private readonly ILogger _logger;
+
+    public TelegramReceiverEndpoint(ILogger logger)
+    {
+      _logger = logger;
+    }
+
+    public bool CanHandle(string messageId, ICorrelationContext context = null)
+      => messageId == TelegramIdentifiers.TELEGRAM_A || messageId == TelegramIdentifiers.TELEGRAM_B;
+
+    public Task<Acknowledgement> HandleAsync(string messageId, object message, ICorrelationContext context = null)
+    {
+      switch (messageId)
+      {
+        case TelegramIdentifiers.TELEGRAM_A:
+          var telegramA = Telegram_A.Create(message);
+          _logger.Information($"Received telegram A with integer field set to {telegramA.Integer_4Byte} using {context.ProtocolId}");
+          break;
+
+        case TelegramIdentifiers.TELEGRAM_B:
+          var telegramB = Telegram_B.Create(message);
+          _logger.Information($"Received telegram B with integer array fields set to [{String.Join(", ", telegramB.Integer_4Byte)}] using {context.ProtocolId}");
+          break;
+      }
+
+      return Task.FromResult<Acknowledgement>(new Ack());
+    }
+  }
+}
---
+++ samples/TelegramHandling/TelegramHandlingEndpoints/TelegramSenderEndpoint.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+using ProconTel.Sdk.Attributes;
+using ProconTel.Sdk.Builders;
+using ProconTel.Sdk.Communications.Middlewares;
+using ProconTel.Sdk.Services;
+using ProconTel.Sdk.StandardEndpoints;
+using TelegramDefinitions.Telegrams;
+
+namespace SendingTelegramsEndpoint
+{
+  [EndpointMetadata(Name = "Telegram Sender", SupportedRoles = SupportedRoles.Provider)]
+  // Declarative way of defining what messages (telegrams) can be send by endpoint
+  [MessageMetadata(TelegramIdentifiers.TELEGRAM_A, "Telegram A")]
+  [MessageMetadata(TelegramIdentifiers.TELEGRAM_B, "Telegram A")]
+  public class TelegramSenderEndpoint : IEndpointLifeTimeCycle
+  {
+    private readonly ILogger _logger;
+    private readonly IMessageBus _messageBus;
+
+    public TelegramSenderEndpoint(ILogger logger, IMessageBus messageBus)
+    {
+      _logger = logger;
+      _messageBus = messageBus;
+    }
+
+    public Task InitializeAsync(IMiddlewareBuilder builder)
+    {
+      Task.Factory
+        .StartNew(SendMessages)
+        .ContinueWith(result =>
+        {
+          if (result.Exception != null)
+            _logger.Error(result.Exception);
+        },
+        TaskContinuationOptions.OnlyOnFaulted);
+
+      return Task.CompletedTask;
+    }
+
+    public Task TerminateAsync()
+    {
+      return Task.CompletedTask;
+    }
+
+    private async Task SendMessages()
+    {
+      for (int i = 1; i <= 10; i++)
+      {
+        await Task.Delay(1000);
+        var telegramA = new Telegram_A(true) { Integer_4Byte = i };
+
+        // Assuming telegram should be received by one of ProconTEL Standard Endpoints, then
+        // XML or Binary protocols should be used. In other cases, any protocol supported by receiver
+        // can be used.
+        await _messageBus.BroadcastAsync(TelegramIdentifiers.TELEGRAM_A, telegramA.GetXml(), new XmlProtocol());
+        //_messageBus.Broadcast(TelegramIdentifiers.TELEGRAM_A, telegramA.GetBytes(), new BinaryProtocol());
+      }
+    }
+  }
+}
